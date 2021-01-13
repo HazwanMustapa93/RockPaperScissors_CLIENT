c90f81aa2d4496a5ae6bf41afbdeb048c1a6627f
Hazwan <hazwanmustapa93@gmail.com.my>
2021-01-13 22:01:56 +0800

    addded

---
+++ RockPaperScissors/Models/GameRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RockPaperScissors.Models
+{
+    public class GameRule
+    {
+        public string InputA { get; set; }
+        public string InputB { get; set; }
+        public string Winner { get; set; }
+    }
+}
---
+++ RockPaperScissors/Services/GameService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RockPaperScissors.Services
+{
+    public class GameService
+    {
+    }
+}
